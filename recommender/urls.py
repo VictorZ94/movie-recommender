1377558134fce393e4ec8143f49b4e5b0c74f764
Victor Zuluaga <zrvictor@outlook.com>
2023-07-27 11:32:36 -0500

    updated recommender urls

--- recommender/urls.py
+++ recommender/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from movierecommender import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movierecommender/', include('movierecommender.urls'))
+    path('movierecommender/', include('movierecommender.urls')),
+    path('', views.index, name='home')
 ]
