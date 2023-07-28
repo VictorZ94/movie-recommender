4dec89575d93e4751b0ea254df2f1483ac128ec7
Victor Zuluaga <zrvictor@outlook.com>
2023-07-28 18:12:41 -0500

    improve static files

--- static/src/script.js
+++ static/src/script.js
@@ -1,5 +1,6 @@
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+let pathName = window.location.pathname;
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -39,10 +40,30 @@ themeToggleBtn.addEventListener('click', function() {
     
 });
 
+const likedButton = document.querySelectorAll('.heart-button');
+const heartLike = document.getElementById('like-heart');
+const heartUnlike = document.getElementById('unlike-heart');
 
-// Toggle button like
-var likedButton = document.querySelectorAll('.heart');
+// likedButton.forEach(ele => ele.addEventListener('click', function(e) {
+//     e.preventDefault();
+// }));
 
-likedButton.addEventListener('click', function() {
-    likedButton.classList.toggle('is-active');
-});
+// Path name
+
+const navbarLinks =  document.querySelectorAll('.nav-link');
+
+if (pathName === '/movierecommender/') {
+    navbarLinks[1].classList.add('text-blue-700');
+    navbarLinks[1].classList.add('!dark:text-blue-700');
+} else {
+    navbarLinks[1].classList.remove('text-blue-700');
+    navbarLinks[1].classList.remove('!dark:text-blue-700');
+}
+
+if (pathName === '/') {
+    navbarLinks[0].classList.add('text-blue-700');
+    navbarLinks[0].classList.add('!dark:text-blue-700');
+} else {
+    navbarLinks[0].classList.remove('text-blue-700');
+    navbarLinks[0].classList.remove('!dark:text-blue-700');
+}
