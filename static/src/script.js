95e3765fa4e6a90ed2f065707639ce28d0bfb61d
Victor Zuluaga <zrvictor@outlook.com>
2023-07-28 22:11:46 -0500

    new styles and js ligic added

--- static/src/script.js
+++ static/src/script.js
@@ -3,8 +3,9 @@ var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 let pathName = window.location.pathname;
 
 // Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+if (localStorage.getItem('color-theme') === 'dark') {
     themeToggleLightIcon.classList.remove('hidden');
+    document.documentElement.classList.add('dark');
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
 }
